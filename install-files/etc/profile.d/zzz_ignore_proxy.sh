5abc6609c1f55fa463f4ac84883b987ade25e2a6
cless <hectorgh@gmail.com>
2019-07-22 13:44:01 +0200

    trying to make sure proxy is disabled if connection fails

--- install-files/etc/profile.d/zzz_ignore_proxy.sh
+++ install-files/etc/profile.d/zzz_ignore_proxy.sh
@@ -1,7 +1,7 @@
 
 eval $(n4d-vars getvalues CLIENT_PROXY_ENABLED | grep CLIENT_PROXY_ENABLED)
 
-if [ "$CLIENT_PROXY_ENABLED" = "False" ]; then
+if [ "$CLIENT_PROXY_ENABLED" = "False" -o -z "$CLIENT_PROXY_ENABLED" ]; then
 
 	unset http_proxy
 	unset https_proxy
