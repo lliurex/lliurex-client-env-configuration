f63f52e1f53e3c9b95d019784c05966457b48a82
cless <hectorgh@gmail.com>
2021-06-01 09:50:33 +0200

    changelog updated

--- install-files/etc/profile.d/http_proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export http_proxy="http://proxy:3128"
-export ftp_proxy="http://proxy:3128"
-export https_proxy="http://proxy:3128"
-export no_proxy="localhost,127.0.0.1,127.0.1.1,127.0.0.0"
-
-if [ "$XDG_CURRENT_DESKTOP" != "" ]; then
-	gsettings set org.gnome.system.proxy mode "auto"
-fi
